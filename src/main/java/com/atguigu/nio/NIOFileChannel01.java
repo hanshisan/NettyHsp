a409ce82d40643ab84dcb8c9d5ed4eb7295d3fcb
hansong <hansong5@asiainfo.com>
2020-10-16 00:42:25 +0800

    冗余import去除

--- src/main/java/com/atguigu/nio/NIOFileChannel01.java
+++ src/main/java/com/atguigu/nio/NIOFileChannel01.java
@@ -1,6 +1,5 @@
 package com.atguigu.nio;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
--- src/main/java/com/atguigu/nio/NIOFileChannel02.java
+++ src/main/java/com/atguigu/nio/NIOFileChannel02.java
@@ -1,8 +1,6 @@
 package com.atguigu.nio;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
